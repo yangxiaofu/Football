fc361d759bbdc3f5ab183ae8f3fe38031068f38f
Dave Dong <fudaviddong@Dave-Dongs-Mac-Mini.local>
2016-08-17 16:54:07 -0700

    First commit

---
+++ Assets/Scripts/FieldManager.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldManager : MonoBehaviour {
+
+	enum State{
+		FirstDown, SecondDown, ThirdDown, FourthDown, TouchDown
+	}
+
+	enum Side{
+		Left, Right
+	}
+
+	State myState;
+	
+	Side _side;
+	private int endZoneHome = -50;
+	private int endZoneAway = 50;
+	private int center = 50;
+
+	PossessionManager possessionMgr = new PossessionManager();
+
+	public int programYardLine;
+	public int realYardLine;
+
+	void Start(){
+
+		possessionMgr.SetPossession (Possession.Away);
+
+		_side = Side.Right;
+
+		SetYardLine  (40, _side);
+
+		//Test Cases for moving the ball.
+		if (myState != State.TouchDown) {
+			YardsGained (Random.Range(0, 15));
+
+			print ("Current Yardline " + this.realYardLine);
+			print ("Side " + _side);
+		}
+		if (myState != State.TouchDown) {
+			YardsGained (Random.Range(0, 15));
+			print ("Current Yardline " + this.realYardLine);
+			print ("Side " + _side);
+		}
+		if (myState != State.TouchDown) {
+			YardsGained (Random.Range(0, 15));
+			print ("Current Yardline " + this.realYardLine);
+			print ("Side " + _side);
+		}
+		if (myState != State.TouchDown) {
+			YardsGained (Random.Range(0, 15));
+			print ("Current Yardline " + this.realYardLine);
+			print ("Side " + _side);
+		}
+	}
+
+	void SetYardLine(int yardline, Side _side){
+		//convert to the program yardline in the background. 
+		if (_side == Side.Left) {
+			this.programYardLine = -(center - yardline);
+		} else if (_side == Side.Right) {
+			this.programYardLine = center - yardline;
+		}
+
+		this.realYardLine = yardline;
+	}
+
+	void YardsGained(int yards){
+
+		if (possessionMgr.possession == Possession.Home) {
+			AddYards(yards);
+		} else if (possessionMgr.possession == Possession.Away) {
+			AddYards(-yards);
+		}
+
+
+
+		if (isTouchDown()) {
+			myState = State.TouchDown;
+			print ("TOUCHDOWN BABY");
+		}
+	}
+
+	bool isTouchDown(){
+		if (this.programYardLine <= this.endZoneHome) {
+			return true;
+		} else if (this.programYardLine >= this.endZoneAway){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void AddYards(int yards){
+
+		CheckSide (this.programYardLine, yards);
+		//Adjusts the yards
+		this.programYardLine += yards;
+		if (this.programYardLine < 0) {
+			this.realYardLine = this.programYardLine + center;
+		} else if (this.programYardLine > 0) {
+			this.realYardLine = center - this.programYardLine;
+		} else {
+			this.realYardLine = center;
+		}
+
+	}
+
+	private void CheckSide(int prevYards, int yards){
+
+		int newYards = prevYards + yards;
+
+		if ((prevYards <= 0) && (newYards >= 0)) {
+			SwitchSides();
+		} else if ((prevYards >= 0) && (newYards <= 0)){
+			SwitchSides();
+		}
+
+	}
+
+	private void SwitchSides(){
+		if (this._side == Side.Left) {
+			this._side = Side.Right;
+		} else if (this._side == Side.Right){
+			this._side = Side.Left;
+		}
+	}
+}
---
+++ Assets/Scripts/PossessionManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum Possession {
+	Home, Away
+}
+
+public class PossessionManager {
+
+	public Possession possession;
+	
+	void Start(){
+		possession = Possession.Away;
+	}
+
+
+	public void SetPossession(string pos){
+		if (pos == "Away") {
+			possession = Possession.Away;
+		} else {
+			possession = Possession.Home;
+		}
+	}
+
+	public void SetPossession(Possession pos){
+		if (pos == Possession.Away) {
+			this.SetPossession ("Away");
+		} else {
+			this.SetPossession ("Home");
+		}
+
+	}
+	
+	public void ChangePossession(){
+		if (possession == Possession.Away) {
+
+			possession = Possession.Home;
+
+		} else if (possession == Possession.Home) {
+
+			possession = Possession.Away;
+
+		}
+	}
+}
